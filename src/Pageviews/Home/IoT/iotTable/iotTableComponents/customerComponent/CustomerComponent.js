6b9e4e3be15585c6bdac215a184d0473a60a2c8d
Khalid Mim Muzahid <khalidmimm@gmail.com>
2024-05-14 18:03:43 +0600

    Test

--- src/Pageviews/Home/IoT/iotTable/iotTableComponents/customerComponent/CustomerComponent.js
+++ src/Pageviews/Home/IoT/iotTable/iotTableComponents/customerComponent/CustomerComponent.js
@@ -1,4 +1,3 @@
-import { Box, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useGetSensorModuleByMacAddressQuery } from "../../../../../../features/iot/iotSlice";
 import { useGetCustomerDetailsMiniQuery } from "../../../../../../features/customers/customersSlice";
